1e76e66ea7684e57bf9f576f055afd9d524780cd
Austin Bingham <austin.bingham@emerson.com>
2013-10-02 16:00:59 +0200

    added setup support

---
+++ setup.py
@@ -0,0 +1,37 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'yapga',
+    version = '1',
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = 'Austin Bingham',
+    author_email = 'austin.bingham@gmail.com',
+    description = "Yet Another Python Gerrit API",
+    license = 'MIT',
+    keywords = 'gerrit',
+    url = 'http://github.com/abingham/yapga',
+    # download_url = '',
+    long_description = 'An API for working with Gerrit from Python via the REST API.',
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+    platforms='any',
+    setup_requires=[],
+    install_requires=[],
+
+    entry_points = {
+        'console_scripts': [
+            #'yapga = yapga.app:main',
+            ],
+        },
+    )
