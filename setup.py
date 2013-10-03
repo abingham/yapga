794334e0df042b14ff297962af6f3709a988cb7f
Austin Bingham <austin.bingham@emerson.com>
2013-10-03 12:56:49 +0200

    added baker to setup

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-    name = 'yapga',
-    version = '1',
-    packages = find_packages(),
+    name='yapga',
+    version='1',
+    packages=find_packages(),
 
     # metadata for upload to PyPI
-    author = 'Austin Bingham',
-    author_email = 'austin.bingham@gmail.com',
-    description = "Yet Another Python Gerrit API",
-    license = 'MIT',
-    keywords = 'gerrit',
-    url = 'http://github.com/abingham/yapga',
+    author='Austin Bingham',
+    author_email='austin.bingham@gmail.com',
+    description="Yet Another Python Gerrit API",
+    license='MIT',
+    keywords='gerrit',
+    url='http://github.com/abingham/yapga',
     # download_url = '',
-    long_description = 'An API for working with Gerrit from Python via the REST API.',
+    long_description='An API for working with Gerrit '
+                     'from Python via the REST API.',
     zip_safe=True,
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -24,14 +25,16 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        ],
+    ],
     platforms='any',
     setup_requires=[],
-    install_requires=[],
+    install_requires=[
+        'baker',
+    ],
 
-    entry_points = {
+    entry_points={
         'console_scripts': [
             #'yapga = yapga.app:main',
-            ],
-        },
-    )
+        ],
+    },
+)
