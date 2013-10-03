a02a0b565fef7eff5a4004257f15de536ad5c6c3
Austin Bingham <austin.bingham@emerson.com>
2013-10-03 13:02:15 +0200

    app using baker

--- yapga/app.py
+++ yapga/app.py
@@ -1,22 +1,25 @@
 import itertools
 
+import baker
+
 from yapga import changes, create_connection
-import yapga.user_settings
 
 
-def list_changes():
-    conn = create_connection(yapga.user_settings.url,
-                             yapga.user_settings.username,
-                             yapga.user_settings.password)
+@baker.command
+def list_changes(url, username=None, password=None):
+    conn = create_connection(url,
+                             username,
+                             password)
     for c in itertools.islice(changes(conn), 100):
         print(c.data)
 
 
-def list_all_revisions():
-    conn = create_connection(yapga.user_settings.url,
-                             #yapga.user_settings.username,
-                             #yapga.user_settings.password
-                             )
+@baker.command(default=True)
+def list_all_revisions(url, username=None, password=None):
+    conn = create_connection(url,
+                             username,
+                             password)
+
     for c in itertools.islice(changes(conn), 100):
         print('change-id:', c.id)
 
@@ -24,4 +27,4 @@ def list_all_revisions():
             print(r.id, r.data)
 
 if __name__ == '__main__':
-    list_all_revisions()
+    baker.run()
